82d568860ee0154b1e8b52aa2ca519dce334e003
pakchris <pakchrisweb@gmail.com>
2017-11-29 00:58:49 -0500

    initial css

---
+++ index.js
@@ -0,0 +1,7 @@
+// var 
+// 	scores = [0, 0],
+// 	roundScore = 0,
+// 	activePlayer = 0
+// 	dice = Math.floor(Math.random() * 6) + 1;
+
+// document.querySelector('#player1current').textContent = dice;
