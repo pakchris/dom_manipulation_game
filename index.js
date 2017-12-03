c3d7153e56dc9cfeee09a38177eb487680323cc1
pakchris <pakchrisweb@gmail.com>
2017-12-02 22:40:22 -0500

    dom

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-// var 
-// 	scores = [0, 0],
-// 	roundScore = 0,
-// 	activePlayer = 0
-// 	dice = Math.floor(Math.random() * 6) + 1;
+var scores, roundScore, activePlayer, dice;
 
-// document.querySelector('#player1current').textContent = dice;
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 1;
+dice = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector('#player'+activePlayer+'current p').textContent = dice;
+// document.querySelector('#player'+activePlayer+'current p').innerHTML = '<em>'+dice+'</em>';
