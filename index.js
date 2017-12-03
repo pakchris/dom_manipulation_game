d2f3f717343cb14ea196885e6d187d45dff613d3
pakchris <pakchrisweb@gmail.com>
2017-12-03 02:25:44 -0500

    progress

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ dice = Math.floor(Math.random() * 6) + 1;
 
 document.querySelector('#player'+activePlayer+'current p').textContent = dice;
 // document.querySelector('#player'+activePlayer+'current p').innerHTML = '<em>'+dice+'</em>';
+
+var playerScore = document.querySelector('#player'+activePlayer+'score').textContent;
+console.log(playerScore);
+
+
+
+
