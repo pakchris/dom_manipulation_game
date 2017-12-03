b03e66319484b1159e072cf381b987cd1920c59c
pakchris <pakchrisweb@gmail.com>
2017-12-03 03:18:35 -0500

    more progress

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 1;
-dice = Math.floor(Math.random() * 6) + 1;
 
-document.querySelector('#player'+activePlayer+'current p').textContent = dice;
-// document.querySelector('#player'+activePlayer+'current p').innerHTML = '<em>'+dice+'</em>';
+// document.querySelector('#player'+activePlayer+'current p').textContent = dice; // document.querySelector('#player'+activePlayer+'current p').innerHTML = '<em>'+dice+'</em>';
 
-var playerScore = document.querySelector('#player'+activePlayer+'score').textContent;
-console.log(playerScore);
+// var playerScore = document.querySelector('#player'+activePlayer+'score').textContent;
 
+document.querySelector('#dice').style.display = 'none';
 
+document.querySelector('#rolldice').addEventListener('click', rollFunc); // document.querySelector('#rolldice').addEventListener('click', function(){});
+
+function rollFunc(){
+	var dice = Math.floor(Math.random() * 6) + 1;
+	// display result
+	var diceDom= document.querySelector('#dice');
+	diceDom.style.display = 'block';
+	diceDom.src = 'dice-'+dice+'.png';
+	// update round score if rolled # is not 1
+
+}
 
 
